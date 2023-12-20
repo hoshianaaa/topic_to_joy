931ca28dafa550f13137a2d34ec7a533950a4b7b
hoshianaaa <ryotaro.hoshina@gmail.com>
2023-12-20 12:36:18 +0900

    update

--- scripts/topic_to_joy.py
+++ scripts/topic_to_joy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import Int32, Float64
+from std_msgs.msg import Bool, Float64
 from sensor_msgs.msg import Joy
 
 class Topic2Joy:
@@ -17,23 +17,23 @@ class Topic2Joy:
 
         node_name = rospy.get_name()
 
-        self.bs1 = rospy.Subscriber(node_name + '/button1', Int32, self.button_callback1)
-        self.bs2 = rospy.Subscriber(node_name + '/button2', Int32, self.button_callback2)
-        self.bs3 = rospy.Subscriber(node_name + '/button3', Int32, self.button_callback3)
-        self.bs4 = rospy.Subscriber(node_name + '/button4', Int32, self.button_callback4)
-        self.bs5 = rospy.Subscriber(node_name + '/button5', Int32, self.button_callback5)
+        self.bs1 = rospy.Subscriber(node_name + '/button1', Bool, self.button_callback1)
+        self.bs2 = rospy.Subscriber(node_name + '/button2', Bool, self.button_callback2)
+        self.bs3 = rospy.Subscriber(node_name + '/button3', Bool, self.button_callback3)
+        self.bs4 = rospy.Subscriber(node_name + '/button4', Bool, self.button_callback4)
+        self.bs5 = rospy.Subscriber(node_name + '/button5', Bool, self.button_callback5)
 
-        self.bs6 = rospy.Subscriber(node_name + '/button6', Int32, self.button_callback6)
-        self.bs7 = rospy.Subscriber(node_name + '/button7', Int32, self.button_callback7)
-        self.bs8 = rospy.Subscriber(node_name + '/button8', Int32, self.button_callback8)
-        self.bs9 = rospy.Subscriber(node_name + '/button9', Int32, self.button_callback9)
-        self.bs10 = rospy.Subscriber(node_name + '/button10', Int32, self.button_callback10)
+        self.bs6 = rospy.Subscriber(node_name + '/button6', Bool, self.button_callback6)
+        self.bs7 = rospy.Subscriber(node_name + '/button7', Bool, self.button_callback7)
+        self.bs8 = rospy.Subscriber(node_name + '/button8', Bool, self.button_callback8)
+        self.bs9 = rospy.Subscriber(node_name + '/button9', Bool, self.button_callback9)
+        self.bs10 = rospy.Subscriber(node_name + '/button10', Bool, self.button_callback10)
 
-        self.bs11 = rospy.Subscriber(node_name + '/button11', Int32, self.button_callback11)
-        self.bs12 = rospy.Subscriber(node_name + '/button12', Int32, self.button_callback12)
-        self.bs13 = rospy.Subscriber(node_name + '/button13', Int32, self.button_callback13)
-        self.bs14 = rospy.Subscriber(node_name + '/button14', Int32, self.button_callback14)
-        self.bs15 = rospy.Subscriber(node_name + '/button15', Int32, self.button_callback15)
+        self.bs11 = rospy.Subscriber(node_name + '/button11', Bool, self.button_callback11)
+        self.bs12 = rospy.Subscriber(node_name + '/button12', Bool, self.button_callback12)
+        self.bs13 = rospy.Subscriber(node_name + '/button13', Bool, self.button_callback13)
+        self.bs14 = rospy.Subscriber(node_name + '/button14', Bool, self.button_callback14)
+        self.bs15 = rospy.Subscriber(node_name + '/button15', Bool, self.button_callback15)
 
         self.as1 = rospy.Subscriber(node_name + '/axis1', Float64, self.axis_callback1)
         self.as2 = rospy.Subscriber(node_name + '/axis2', Float64, self.axis_callback2)
@@ -45,49 +45,49 @@ class Topic2Joy:
 
     # button data callbacks
     def button_callback1(self, msg):
-        self.button_datas[0] = msg.data
+        self.button_datas[0] = int(msg.data)
 
     def button_callback2(self, msg):
-        self.button_datas[1] = msg.data
+        self.button_datas[1] = int(msg.data)
 
     def button_callback3(self, msg):
-        self.button_datas[2] = msg.data
+        self.button_datas[2] = int(msg.data)
 
     def button_callback4(self, msg):
-        self.button_datas[3] = msg.data
+        self.button_datas[3] = int(msg.data)
 
     def button_callback5(self, msg):
-        self.button_datas[4] = msg.data
+        self.button_datas[4] = int(msg.data)
 
     def button_callback6(self, msg):
-        self.button_datas[5] = msg.data
+        self.button_datas[5] = int(msg.data)
 
     def button_callback7(self, msg):
-        self.button_datas[6] = msg.data
+        self.button_datas[6] = int(msg.data)
 
     def button_callback8(self, msg):
-        self.button_datas[7] = msg.data
+        self.button_datas[7] = int(msg.data)
 
     def button_callback9(self, msg):
-        self.button_datas[8] = msg.data
+        self.button_datas[8] = int(msg.data)
 
     def button_callback10(self, msg):
-        self.button_datas[9] = msg.data
+        self.button_datas[9] = int(msg.data)
 
     def button_callback11(self, msg):
-        self.button_datas[10] = msg.data
+        self.button_datas[10] = int(msg.data)
 
     def button_callback12(self, msg):
-        self.button_datas[11] = msg.data
+        self.button_datas[11] = int(msg.data)
 
     def button_callback13(self, msg):
-        self.button_datas[12] = msg.data
+        self.button_datas[12] = int(msg.data)
 
     def button_callback14(self, msg):
-        self.button_datas[13] = msg.data
+        self.button_datas[13] = int(msg.data)
 
     def button_callback15(self, msg):
-        self.button_datas[14] = msg.data
+        self.button_datas[14] = int(msg.data)
 
     # axis data callbacks
     def axis_callback1(self, msg):
